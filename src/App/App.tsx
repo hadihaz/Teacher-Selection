425312845fb2f9d1154b254356379a8e9f881ff8
Hadi <hadihaz1999@gmail.com>
2024-02-14 21:59:15 +0330

    create project

---
+++ src/App/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <h1 className="w-[300px] bg-green-400 text-3xl font-bold underline">
+        سلام
+      </h1>
+    </>
+  );
+}
+
+export default App;
