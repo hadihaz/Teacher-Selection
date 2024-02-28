507e23d58d04f8b1bba20787fc37983002bf6b99
Hadi <hadihaz1999@gmail.com>
2024-02-28 12:28:48 +0330

    ...

--- src/components/common/pagination.tsx
+++ src/components/common/pagination.tsx
@@ -40,7 +40,7 @@ const Pagination: React.FC<IPaginationProps> = ({
             e.preventDefault();
             handlePrevClick();
           }}
-          className="relative inline-flex items-center rounded-l-md px-2 py-1 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+          className="relative inline-flex items-center px-2 py-1 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
         >
           قبلی
         </a>
@@ -92,7 +92,7 @@ const Pagination: React.FC<IPaginationProps> = ({
             e.preventDefault();
             handleNextClick();
           }}
-          className="relative inline-flex items-center rounded-r-md px-2 py-1 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+          className="relative inline-flex items-center px-2 py-1 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
         >
           بعدی
         </a>
